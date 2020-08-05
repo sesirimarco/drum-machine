0d96f1599f04583a359b4f3560d003e6935acc20
sesirimarco <sesirimarco@gmail.com>
2020-08-04 22:18:10 -0300

    Firs commit!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button'
+import { bankOne, matrixBanks } from "./banks";
 
+const Pad = ({id, keyTrigger, keyCode, url, vol, onPlay }) => {
+	const [audio, setAudio] = React.useState(null);
+	const [isPlaying, setIsPlaying] = React.useState(false);
+	React.useEffect(() => {
+		const audio = document.getElementById(keyTrigger);
+		setAudio(audio);
+		audio.addEventListener('ended', (e) => {
+			setIsPlaying(false);
+		});
+		const handlerKeyDown = (e) => {
+			playAudio(e.key);
+		};
+		
+		document.addEventListener('keydown', handlerKeyDown)
+		return () => {
+			document.removeEventListener('keydown', handlerKeyDown);
+		};
+	}, [audio]);
+	const playAudio = (key) => {
+        //audio.currentTime = 0;
+		if(key && key.toUpperCase() === keyTrigger){
+			setIsPlaying(true);
+			audio.play();
+			onPlay(id);
+		} else if (!key) {
+			setIsPlaying(true);
+			audio.play();
+			onPlay(id);
+		}
+	};
+    return (
+        <div 
+			className="drum-pad" 
+			id={id}
+			onClick={(e) => {
+					playAudio();
+				}}
+		>
+            <Button 
+				variant={`outline-dark ${isPlaying ? 'active' : ''} p-4 m-2`}
+			>
+				{keyTrigger}
+			</Button>
+			<audio src={url} className="clip" id={keyTrigger}></audio>
+        </div>
+    );
+};
+
+const DrumMachine = ({vol, onPlay}) => {
+	const banks = matrixBanks(bankOne);
+    return (
+        <div id="drum-machine">
+			{banks.map((row, index) => (
+				<Row className="">
+					{row.map(({ id, keyCode, keyTrigger, url }) => (
+						<Col xs="3" md="3">
+							<Pad
+								id={id}
+								keyCode={keyCode}
+								keyTrigger={keyTrigger}
+								url={url}
+								vol={vol}
+								onPlay={onPlay}
+								/>
+						</Col>
+					))}
+				</Row>
+			))}
+        </div>
+    );
+};
+const App = () => {
+	const [currentBank, setCurrentBank] = React.useState('')
+	return(
+		<div id="container" className="container">
+			<h1>Drum Machine</h1>
+			<Row>
+				<Col>
+					<DrumMachine  vol={1} onPlay={(id) => {
+							setCurrentBank(id);
+						}}/>
+				</Col>	
+				<Col>
+					<div id="display"> { currentBank }</div>
+				</Col>
+			</Row>
+
+		</div>
+	)
+};
 export default App;
